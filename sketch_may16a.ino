0f2365893095e56f237670b9c119405372d29381
ArturoAG <noe_170695@hotmail.com>
2017-05-17 22:23:29 -0500

    primer commit

---
+++ sketch_may16a.ino
@@ -0,0 +1,23 @@
+int relay = 2;
+int relay4 = 4;
+
+// La rutina setup se ejecuta al iniciar el Arduino o al presionar el boton reset
+void setup() {                
+// Inicialica el pin digital relay (que vale 2)  como salida
+  pinMode(relay, OUTPUT);   
+  pinMode(relay4, OUTPUT);  
+}
+
+// La rutina loop se ejecuta en forma infinita despues de inicializado el Arduino
+void loop() {
+  digitalWrite(relay, HIGH);   
+  delay(1000); 
+  digitalWrite(relay4, HIGH);
+  delay(1000);                
+  digitalWrite(relay, LOW);    
+  delay(1000); 
+  digitalWrite(relay4, LOW);
+  delay(1000);   
+  
+  
+}
